7e1ab46f36a527171bbc2fe40efce644940f35e8
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2023-04-16 14:40:09 -0300

    :recycle: added img route and 404
    Co-authored-by: Erick Sousa <ericksantos12@users.noreply.github.com>
    Co-authored-by: Guilherme-Soares-Sousa <Guilherme-Soares-Sousa@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express")
-const router = require("./routes.js")
+const router = require("./routes/routes.js")
+const img_router = require("./routes/img-routes.js")
 const dotenv = require("dotenv").config();
 
 const app = express();
 
-app.use(router)
+app.use(router);
+app.use(img_router);
 
 app.use((req, res) => {
-    res.status(404).send("404 Not Found")
+    res.status(404).sendFile(__dirname + '/view/404.html')
 })
 
 app.set('port', process.env.port || 7777);
